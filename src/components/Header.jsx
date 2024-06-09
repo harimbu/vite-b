0945b83446c322d516592369147c4af85886d2ec
harimbu <46551881+harimbu@users.noreply.github.com>
2024-06-09 23:04:24 +0900

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,49 @@
 import { useState } from "react";
-import { MdInfo  } from "react-icons/md";
+import { MdPerson , MdLogout } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { auth } from "../conf/firebase";
+import { signOut } from "firebase/auth";
 
 export default function Header() {
+  const [email, setEmail] = useState(null)
   const [modal, setModal] = useState(false)
+
+  function handleModal() {
+    setModal(!modal)
+  }
+
+  function handleLogin() {
+
+  }
+
+  function logout() {
+    signOut(auth).then(() => {
+      setEmail(null)
+    })
+  }
+
   return (
     <div className="header">
-      <Link to={'/'}>
-        <h1>🧶🎨💋</h1>
-      </Link>
-      <MdInfo onClick={()=>setModal(!modal)} />
+      <Link to={'/'}><h1>🧶🎨💋</h1></Link>
+      { email
+        ? 
+        <div className="email">{email}<MdLogout onClick={logout} /></div>
+        : 
+        <MdPerson onClick={handleModal} />
+      }
 
-      {modal && (
+      { modal && 
+        (
           <div className="modal">
-            <div className="info">
-              <h2>Lorem ipsum dolor sit amet.</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus non veritatis quam doloremque quisquam, dignissimos quod cupiditate molestiae vero iure iste labore atque. Similique, aliquid quidem ullam culpa est minus a nisi ab quo modi facilis blanditiis maxime reprehenderit eos perspiciatis quae quam beatae! A commodi dolor voluptatem at nesciunt!</p>
-              <button onClick={()=>setModal(!modal)}>확인</button>
-            </div>
+            <form onSubmit={handleLogin}>
+              <h2>로그인</h2>
+              <input type="text" placeholder="이메일" />
+              <input type="text" placeholder="비빌번호" />
+              <div className="btns">
+                <button type="button" onClick={handleModal}>취소</button>
+                <button type="submit">로그인</button>
+              </div>
+            </form>
           </div>
         )
       }
